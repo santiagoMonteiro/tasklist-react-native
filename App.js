511e81321486cc8c713a8da8b018fc7da2ea1cfe
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2022-06-07 16:03:03 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+
+import { FontAwesome } from "@expo/vector-icons";
+
+const [taskName, setTaskName] = useState("");
+
+function handleAddingTask() {
+  alert(taskName);
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Tarefas</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Digite sua tarefa..."
+          style={styles.taskInput}
+          value={taskName}
+          onChangeText={(text) => setTaskName(text)}
+        />
+        <TouchableOpacity
+          style={styles.addTaskButton}
+          onPress={handleAddingTask}
+        >
+          <FontAwesome name="plus" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +40,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#22272e",
+    paddingTop: 28,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    color: "#FFF",
+    marginTop: "5%",
+    paddingStart: "5%",
+    marginBottom: 12,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    width: "100%",
+    height: 44,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 22,
+  },
+  taskInput: {
+    width: "75%",
+    backgroundColor: "#FBFBFB",
+    height: 44,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+  },
+  addTaskButton: {
+    width: "15%",
+    height: 44,
+    backgroundColor: "#73f7ff",
+    marginLeft: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 4,
   },
 });
